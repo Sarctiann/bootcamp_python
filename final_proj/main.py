a49dd0860820f3a0aeeebf6aee4e4df6df43fb0a
Sarctiann (Sebastian Rodriguez) <sebastian.rodriguez@hstpathways.com>
2024-08-02 19:12:11 -0300

    added a templating basic example

--- final_proj/main.py
+++ final_proj/main.py
@@ -1,13 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
-from .routes import roles_router
+from .api.routes import roles_router
 
 app = FastAPI(title="Final Project API")
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
 
 @app.get("/")
-def home():
-    return {"message": "Hello, World!"}
+def get_template(request: Request):
+    return templates.TemplateResponse(
+        name="index.html",
+        request=request,
+        context=dict(
+            title="Final Project API",
+            description='Go to "/docs" for API documentation',
+        ),
+    )
 
 
 app.include_router(roles_router, prefix="/roles", tags=["Roles"])
