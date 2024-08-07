f9256bb51a97a77da72edf3134366a23341a6651
Sarctiann (Sebastian Rodriguez) <sebastian.rodriguez@hstpathways.com>
2024-08-07 11:06:28 -0300

    enhanced templates

--- proyecto_final/main.py
+++ proyecto_final/main.py
@@ -2,34 +2,20 @@ from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-from . import mod
 
-app = FastAPI(title="Nuestra primer App")
+app = FastAPI(title="Final Project API")
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/")
-async def home(request: Request):
+@app.get("/", include_in_schema=False)
+def home(request: Request):
     return templates.TemplateResponse(
-        "index.html",
-        context={"mensaje": mod.text, "request": request},
+        name="index.html",
+        request=request,
+        context=dict(
+            title="Final Project API",
+            description='Go to "/docs" for API documentation',
+        ),
     )
-
-
-obj = dict(
-    mensaje="hola mundo",
-    descripcion="mi primer app",
-    existe=None,
-    booleano=True,
-    tupla={1, 2, 3},
-)
-
-
-def hola():
-    return "hola"
-
-
-@app.get("/about")
-async def about():
-    return hola
